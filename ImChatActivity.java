1e3117234dbdc50b2f65620ad54e66591bfca64e
zhangsan <2909387576@qq.com>
2016-09-01 14:14:05 +0800

    imchat

---
+++ ImChatActivity.java
@@ -0,0 +1,17 @@
+package com.example.im;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ImChatActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_im_chat);
+	}
+
+	
+}
